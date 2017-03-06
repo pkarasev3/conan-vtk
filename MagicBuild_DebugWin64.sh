fbf6270207a622c7fab0fb12f596d3443be33d36
peter karasev <peter.karasev@keysight.com>
2017-03-06 15:05:08 -0500

    adjsut files

--- MagicBuild_DebugWin64.sh
+++ MagicBuild_DebugWin64.sh
@@ -1,1 +1,2 @@
-conan install VTK/7.1.0@pkarasev/testing -s build_type=Debug -s "compiler=Visual Studio" -s compiler.runtime=MDd  --build VTK
+conan install VTK/7.1.0@pkarasev/testing -s build_type=Debug -s "compiler=Visual Studio" -s compiler.runtime=MDd\
+      -o shared=True -o linux_use_sudo=False -o cygwin_msvc=False --build VTK
--- MagicBuild_ReleaseWin64.sh
+++ MagicBuild_ReleaseWin64.sh
@@ -1,1 +1,2 @@
-conan install VTK/7.1.0@pkarasev/testing -s build_type=Release -s "compiler=Visual Studio" -s compiler.runtime=MD  --build VTK
+conan install VTK/7.1.0@pkarasev/testing -s build_type=Release -s "compiler=Visual Studio" -s compiler.runtime=MD  \
+           -o shared=True -o linux_use_sudo=False -o cygwin_msvc=False --build VTK
